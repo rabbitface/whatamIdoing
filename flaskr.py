5e85ab588a23a317dc11d4ebbdb67cf89c22744f
claudiay <luper@luperlabs.com>
2014-01-03 16:27:53 -0800

    removed flaskr.py~ file, and fixed formatting

--- flaskr.py
+++ flaskr.py
@@ -10,18 +10,20 @@ DEBUG = True
 SECRET_KEY = 'development key'
 USERNAME = 'admin'
 PASSWORD = 'default'
+
 # create our little application :)
 app = Flask(__name__)
 app.config.from_object(__name__)
+
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
-if __name__ == '__main__':
-    app.run(host='127.0.0.1')
+
 def init_db():
     with closing(connect_db()) as db:
         with app.open_resource('schema.sql', mode='r') as f:
             db.cursor().executescript(f.read())
         db.commit()
+
 @app.before_request
 def before_request():
     g.db = connect_db()
@@ -31,11 +33,13 @@ def teardown_request(exception):
     db = getattr(g, 'db', None)
     if db is not None:
         db.close()
+
 @app.route('/')
 def show_entries():
     cur = g.db.execute('select title, text from entries order by id desc')
     entries = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
     return render_template('show_entries.html', entries=entries)
+
 @app.route('/add', methods=['POST'])
 def add_entry():
     if not session.get('logged_in'):
@@ -45,6 +49,7 @@ def add_entry():
     g.db.commit()
     flash('New entry was successfully posted')
     return redirect(url_for('show_entries'))
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     error = None
@@ -58,8 +63,15 @@ def login():
             flash('You were logged in')
             return redirect(url_for('show_entries'))
     return render_template('login.html', error=error)
+
 @app.route('/logout')
 def logout():
     session.pop('logged_in', None)
     flash('You were logged out')
     return redirect(url_for('show_entries'))
+
+
+# This always goes at the bottom of the file
+# What is ever inside this if statement runs automatically when file is running
+if __name__ == '__main__':
+    app.run(host='127.0.0.1')
