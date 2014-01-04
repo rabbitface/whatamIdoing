6a5134f59a64fccbbecd51fe8e1809eead6c8e35
claudiay <luper@luperlabs.com>
2014-01-03 16:46:49 -0800

    opps, here is the config file, i forgot to add it in previous commit

---
+++ configs.py
@@ -0,0 +1,10 @@
+# Config file for flaskr
+# Put all the variables here
+
+# configuration
+DATABASE = '/tmp/flaskr.db'
+DEBUG = True
+SECRET_KEY = 'development key'
+USERNAME = 'admin'
+PASSWORD = 'default'
+
